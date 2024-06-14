0dbd315ce2752afa84444e24f4874679db734544
Blaise <blaiseaganzepascal10@gmail.com>
2024-06-14 05:54:00 -0700

    new repository

--- app.js
+++ app.js
@@ -60,11 +60,36 @@ const contactForm = document.getElementById('contact_form'),
         
         })
   }
+  contactForm,addEventListener('submit',sendEmail)
 /*=============== SHOW SCROLL UP ===============*/ 
-contactForm,addEventListener('submit',sendEmail)
+const scrollUp =() =>{
+  const scrollUp = document.getElementById('scroll-up')
+  //when the scroll is higher than  350 viewport hight , add the
+  this.scrollY >= 350 ? scrollUp.classList.add('show-sroll')
+                      : scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
 
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
 
+const scrollActive =()=>{
+  const scrollDown = window.scrollY
+
+  sections.forEach(current =>{
+    const sectionHeight = current.offsetHeight,
+    sectionTop = current.offsetTop - 58,
+    sectionId = current.getAttribute('id'),
+    sectionsClass = document.querySelector('.nav_menu a[href*=' + sectionId +']')
+
+    if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+      sectionsClass.classList.add('active-link')
+    }else{
+      sectionsClass.classList.remove('active-link')
+    }
+  })
+}
+window.addEventListener('scroll', scrollActive)
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
